c0eb3bef1704e0e5bf1523bcf3d43a72043393f9
Raquel <quelcaraquel1@gmail.com>
2023-02-08 17:59:56 -0400

    Catalogo institucion

---
+++ html/sib/webapp/app/sib/module/institucion/snippet/general/Catalog.php
@@ -0,0 +1,25 @@
+<?PHP
+namespace App\Sib\Institucion\general;
+use Core\CoreResources;
+
+class Catalog extends CoreResources{
+
+    function __construct(){
+        /**
+         * Inicializamos todas las librerias y variables para el submodulo
+         */
+        $this->appInit();
+    }
+
+    public function confCatalog(){
+        $this->addCatalogList($this->table["pais"]
+            ,"pais","","nombre",""
+            ,"nombre","","","");
+
+        $where = "cod_dep <> '0' ";
+        $this->addCatalogList($this->table["departamento"]
+            ,"departamento","","name",""
+            ,"name",$where,"","");
+    }
+
+}
